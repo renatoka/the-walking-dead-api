1030b1e1055df5ef5e19f30375aae78360b194e5
Renato Kauric <rkauric@vvg.hr>
2022-11-18 21:08:01 +0100

    Created character schema

---
+++ backend/models/character.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const characterSchema = new mongoose.Schema({
+    id : {type: Number, required: true},
+    Name : {type: String, required: true},
+    Gender : {type: String, required: true},
+    Image : {type: String, required: true},
+    Hair : {type: String, required: false},
+    FirstAppearance : {type: String, required: false},
+    LastAppearance : {type: String, required: false},
+    CauseOfDeath : {type: String, required: false},
+    DeathEpisode : {type: String, required: false},
+    Status : {type: String, required: true},
+    Ethnicity : {type: String, required: false}
+});
+
+module.exports = mongoose.model('Character', characterSchema);
