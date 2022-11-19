0ad484c82910a5688ce59a99f0fc87eb4881c3f2
Renato Kauric <rkauric@vvg.hr>
2022-11-19 12:42:35 +0100

    Changed routing link to /api

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,11 @@ const characterRouter = require('./routes/characters')
 const PORT = 3000
 
 const app = express();
+app.use(express.json())
 
 mongoose.connect(dotenv.config().parsed.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-
-app.use('/characters', characterRouter)
+app.use('/api', characterRouter)
