cf751df00c48a7cff879ab58498b07fd90e865a8
Renato Kauric <rkauric@vvg.hr>
2022-11-20 16:06:18 +0100

    Fixed issue with rendering page

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const path = require('path');
 const characterRouter = require('./routes/characters')
 const PORT = process.env.PORT || 5000
-
 const app = express();
-app.use(express.json())
-app.use(cors())
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/dist')))
+app.use(cors())
 
 
 mongoose.connect(dotenv.config().parsed.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
