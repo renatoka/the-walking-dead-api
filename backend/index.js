1228ad8adbf41224e04986e91dcff869df3c169a
Renato Kauric <rkauric@vvg.hr>
2022-11-20 12:14:05 +0100

    Added more functionalities

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const characterRouter = require('./routes/characters')
 const PORT = process.env.PORT || 5000
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect(dotenv.config().parsed.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
