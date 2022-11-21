7dcbbfb57fd04472df9042de22431078e9fa6b70
Renato <rkauric@vvg.hr>
2022-11-21 17:35:52 +0100

    New route added

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,13 @@ const characterRouter = require('./routes/characters')
 const PORT = process.env.PORT || 5000
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(cors())
-
 
 mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-app.use('/' , express.static(path.join(__dirname, '/dist')))
-app.use('/api', characterRouter)
+app.use('/api/characters', characterRouter)
