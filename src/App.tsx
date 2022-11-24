8f6fbbb74bab7074e148a2b171f448a4c451494f
Renato <rkauric@vvg.hr>
2022-11-24 18:46:08 +0100

    Imported Quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Main from './components/Main';
-import {Characters} from './components/Characters';
+import { Characters } from './components/Characters';
+import { Quotes } from './components/Quotes';
 import {Routes, Route} from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path='/characters' element={<Characters />} />
+        <Route path='/quotes' element={<Quotes />} />
       </Routes>
     </div>
   );
