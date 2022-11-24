0c66d6b83817e2307f6f2e5db0e521209965bb20
Renato <rkauric@vvg.hr>
2022-11-24 15:04:48 +0100

    Added get method for quotes

---
+++ backend/routes/quotes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const Quote = require("../models/quote");
+
+// Get all quotes with optional parameters
+router.get("/", async (req, res) => {
+  try {
+    const { author, season, episode, id, limit } = req.query
+    if (author || season || episode || id || limit) {
+      const quotes = await Quote.find({
+        author: new RegExp(author, "i"),
+        season: season ? season : { $exists: true },
+        episode: new RegExp(episode, "i"),
+        id: id ? id : { $exists: true },
+      }).limit(parseInt(limit))
+      if (!quotes) {
+        return res.status(404).send("No quotes found. Try again later.")
+      }
+      res.send(quotes)
+    } else {
+      const quotes = await Quote.find()
+      res.send(quotes)
+    }
+  } catch (error) {
+    res.status(400).send({ message: error.message })
+  }
+})
+
+// Used to post many quotes at once
+router.post("/", async (req, res) => {
+  try {
+    const quotes = await Quote.insertMany(req.body);
+    res.status(201).json(quotes);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
